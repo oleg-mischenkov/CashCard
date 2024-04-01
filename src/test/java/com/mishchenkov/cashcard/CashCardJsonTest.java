a37ab3f333e5d333ad47a918f9ab76362d28799d
Oleg Mischenkov <mischenkov.oleg@gmail.com>
2024-04-01 15:05:11 +0200

    failed test

---
+++ src/test/java/com/mishchenkov/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package com.mishchenkov.cashcard;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        Assertions.assertThat(1).isEqualTo(42);
+    }
+}
