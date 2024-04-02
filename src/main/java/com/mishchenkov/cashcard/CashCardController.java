164ea175f3c5963bad5c7cdbee0c8910fec60438
Oleg Mischenkov <mischenkov.oleg@gmail.com>
2024-04-02 22:59:58 +0200

    was added post controller

--- src/main/java/com/mishchenkov/cashcard/CashCardController.java
+++ src/main/java/com/mishchenkov/cashcard/CashCardController.java
@@ -1,14 +1,15 @@
 package com.mishchenkov.cashcard;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/cashcards")
@@ -25,6 +26,18 @@ public class CashCardController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createCashCard(@RequestBody CashCard requestedCashCard, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(requestedCashCard);
+        log.info("### Saved CashCard: {}", savedCashCard);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        log.info("### URI: {}", locationOfNewCashCard);
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 
 }
--- src/test/java/com/mishchenkov/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/mishchenkov/cashcard/CashCardApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -41,7 +43,11 @@ class CashCardApplicationTests {
 	void shouldCreateANewCashCard() {
 		CashCard newCashCard = new CashCard(null, 250.00);
 		ResponseEntity<Void> responseEntity = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
-		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI createdLocation = responseEntity.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(createdLocation, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
